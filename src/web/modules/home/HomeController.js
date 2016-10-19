caf547d3a0327233f1ce621b907e735d7f653584
Amitai Handler <amitaihandler@gmail.com>
2016-10-19 23:45:54 +0300

    working on the user table on the 'home'

--- src/web/modules/home/HomeController.js
+++ src/web/modules/home/HomeController.js
@@ -3,14 +3,19 @@
 angular.module('Home')
  
 .controller('HomeController',
-    ['$scope','UploadFileService',
-    function ($scope, UploadFileService) {
+    ['$scope',
+    function ($scope) {
+        $scope.UserList = [
+            {name: "Idushi", type: 'Human'},
+            {name: "Amitai", type: 'Computer'}
+            ];
+
         $scope.uploadFile = function () {
-            UploadFileService.UploadFile();
+          //  UploadFileService.UploadFile();
         };
 
         $scope.uploadGame = function () {
-            LoadGameService.UploadGame();
+         //   LoadGameService.UploadGame();
         };
       
     }]);
