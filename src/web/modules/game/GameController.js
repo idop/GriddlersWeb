d6ad980785ec3277ecba0931c98045b50f29ba0d
Amitai Handler <amitaihandler@gmail.com>
2016-10-22 22:00:41 +0300

    saving

--- src/web/modules/game/GameController.js
+++ src/web/modules/game/GameController.js
@@ -8,5 +8,27 @@ angular.module('Game')
     .controller('GameController',
         ['$scope',
             function ($scope) {
+                $scope.Game = [];
+                $scope.PlayerList = [];
+                $scope.rowConstraints = [];
+                $scope.columnConstraints = [];
+
+                $scope.Game = {
+                    round :'10',
+                    playerMove : '1',
+                    playerScore: '98',
+                    currentGameStatus: 'The world is going to shits..'
+                };
+
+                $scope.PlayerList = [
+                    { name: "Idushi", type: "Human", score: "37"},
+                    { name: "Amitai", type: "Human", score: "98"},
+                    { name: "HAL", type: "Computer", score: "69"}
+                ]
+
+                $scope.rowConstraints = [
+
+                ]
+
 
             }]);
